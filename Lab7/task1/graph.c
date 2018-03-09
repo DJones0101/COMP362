0b4c1140ffb604af504924b24c19c4bb24ff823d
Darius <darius.jones1018@gmail.com>
2018-03-09 01:55:20 -0800

    Implemented add_edge, remove_edge, free_names.

--- Lab7/task1/graph.c
+++ Lab7/task1/graph.c
@@ -12,7 +12,7 @@
 #define STRING_MAX_SIZE 50
 #define MAX_NUMBER_OF_VERTICES 20
 
-enum {ADD_VERTEX = 0, ADD, ADD_EDGE, REMOVE_EDGE, DISPLAY, CHECK, HELP, QUIT, INPUT_ERROR} commands;
+enum {ADD_VERTEX = 0, ADD_EDGE, REMOVE_EDGE, DISPLAY, CHECK, HELP, QUIT, INPUT_ERROR} commands;
 char *vertex_names[MAX_NUMBER_OF_VERTICES];
 bool adjacency_matrix[MAX_NUMBER_OF_VERTICES][MAX_NUMBER_OF_VERTICES];
 int number_of_vertices = 0;
@@ -34,6 +34,12 @@ void free_names();
 int main(int argc, char *argv[]) {
 	int count;
 	for (count = 0; count < STRING_MAX_SIZE; count++) {vertex_names[count] = malloc(sizeof(char) * STRING_MAX_SIZE);}
+	int row, col;
+	for (row = 0; row < MAX_NUMBER_OF_VERTICES; row++) {
+		for (col = 0; col < MAX_NUMBER_OF_VERTICES; col++) {
+			adjacency_matrix[row][col] = false;
+		}
+	}
 	int index;
 
 	while (true) {
@@ -43,40 +49,35 @@ int main(int argc, char *argv[]) {
 
 		switch (get_command_indexes(commands_in)) {
 
-		case ADD:
-
-			fscanf(stdin, "%s", command1);
-			index = get_command_indexes(command1);
-
-			if ( index == ADD_VERTEX) { // add vertex Aaa
-				//memset(&command2[0], 0, sizeof(command2));
-				fscanf(stdin, "%s", command2);
-				add_vertex(command2);
-
-
-			} else if (index == ADD_EDGE) { // add edge Aaa Bbbb
-
-				fscanf(stdin, "%s %s", command2, command3);
-				add_edge(command2, command3);
-			} else if (index == INPUT_ERROR) {
-				printf("Invalid Command\n"); break;
-			}
-
-			break;
-		case REMOVE_EDGE: break; //fscanf(stdin,"%s %s")  get the next 2 strings call remove_edge() break;
+		case ADD_VERTEX: fscanf(stdin, "%s", command1); add_vertex(command1); break;
+		case ADD_EDGE: fscanf(stdin, "%s %s", command1, command2); add_edge(command1, command2); break;
+		case REMOVE_EDGE: fscanf(stdin, "%s %s", command1, command2); remove_edge(command1, command2); break;
 		case DISPLAY: display_adjacency_matrix(); break;
 		case CHECK: break; //if(check_for_cycle() == true){printf(">> DEADLOCKED <<\n")}else{printf(">> NO DEADLOCK <<\n")};
 		case HELP: display_help(); break;
 		case QUIT: printf("Exiting the program.\n"); exit(EXIT_SUCCESS);
 		default: printf("Invalid Command\n"); break;
+
 		}
 	}
+
+	free_names(vertex_names);
 
 	return 0;
 }
 
 int get_index_of_vertex(char *vertex_name) {
 
+	int count;
+	int return_val = -1;
+	for (count = 0; count < MAX_NUMBER_OF_VERTICES; count++) {
+		if (strcmp(vertex_name, vertex_names[count]) == 0) {
+			return_val = count;
+			break;
+		}
+	}
+
+	return return_val;
 }
 
 void add_vertex(char *vertex_name) {
@@ -95,6 +96,7 @@ void add_vertex(char *vertex_name) {
 
 		}
 		strcpy(vertex_names[number_of_vertices], vertex_name);
+		printf("Added a new vertex named %s at index %d.\n", vertex_name, number_of_vertices);
 		number_of_vertices++;
 	}
 
@@ -103,20 +105,56 @@ void add_vertex(char *vertex_name) {
 
 void add_edge(char *from_vertex_name, char *to_vertex_name) {
 
-	// get the index of from_vertex and to_vertex and find it's place
-	// int the adjacency_matrix
+	int index1 = get_index_of_vertex(from_vertex_name);
+	int index2 = get_index_of_vertex(to_vertex_name);
+
+	if (index1 == index2) {
+		printf("A vertex cannot be connected to itself.\n");
+		return;
+	}
+
+	if (index2 != -1 || index1 !=  -1) {
+
+		bool result = adjacency_matrix[index1][index2];
+		if (result == false) {
+			adjacency_matrix[index1][index2] = true;
+			printf("Added edge from vertex %s to vertex %s.\n", from_vertex_name, to_vertex_name);
+		} else {
+			printf("That edge already exists.\n");
+		}
+
+	} else {
+		printf("One or more of those vertices do not exist.\n");
+	}
+
 
 }
 
 void remove_edge(char *from_vertex_name, char *to_vertex_name) {
 
-	// "maybe"
-	// get the from_vertex and the to_vertex
-	// then remove it from adjacency_matrix maybe replace 1 with 0
-	// remove it from the vertex_names array
+
+	int index1 = get_index_of_vertex(from_vertex_name);
+	int index2 = get_index_of_vertex(to_vertex_name);
+
+	if (index2 != -1 || index1 !=  -1) {
+
+		bool result = adjacency_matrix[index1][index2];
+		if (result == true) {
+			adjacency_matrix[index1][index2] = false;
+			printf("Removed the edge from vertex %s to vertex %s.\n", from_vertex_name, to_vertex_name);
+		} else {
+			printf("No edge exists between those two vertices.\n");
+		}
+
+	} else {
+		printf("One or more of those vertices do not exist.\n");
+	}
 }
 
+
 void display_adjacency_matrix() {
+
+	// needs work
 
 	if (number_of_vertices == 0) {
 
@@ -126,26 +164,27 @@ void display_adjacency_matrix() {
 	} else {
 
 		int count;
-		
+		printf("\t\t");
+
 		for (count = 0; count < number_of_vertices; count++) {
-			printf("\t");
-			printf("%-10s", vertex_names[count]);
-		}printf("\n");
+
+			printf("  %10s", vertex_names[count]);
+		} printf("\n");
 
 		int row;
 		int col;
 		count = 0;
 		for (row = 0; row < number_of_vertices; row++) {
-			printf("\t");
-			printf("%s ",vertex_names[count]);
+
+			printf("\t%10s", vertex_names[count]);
 
 			for (col = 0; col < number_of_vertices; col++) {
 
-				
+
 				if (adjacency_matrix[row][col] == true) {
-					printf("%-10d", 1);
+					printf("%10d", 1);
 				} else {
-					printf("%-10d", 0);
+					printf("%10d", 0);
 				}
 
 			}
@@ -157,22 +196,36 @@ void display_adjacency_matrix() {
 }
 
 bool check_for_cycle() {
+	int row, col;
+	bool previous;
 
+	for(row = 1; row < number_of_vertices; row++){
+		adjacency_matrix[row][col];
+		for(col = 0; col < number_of_vertices; col++){
+			if(){
+
+			}
+		}
+	}
 }
 
 int get_command_indexes(char *commands) {
 
-	if (strcmp(commands, "vertex") == 0) {
+	if (strcmp(commands, "add") == 0) {
 
-		return ADD_VERTEX;
+		fscanf(stdin, "%s", commands);
+		if (strcmp(commands, "vertex") == 0) {
+			return ADD_VERTEX;
+		} else if (strcmp(commands, "edge") == 0) {
+			return ADD_EDGE;
+		}
 
-	} else if ( strcmp(commands, "edge") == 0) {
+	} else if ( strcmp(commands, "remove") == 0) {
 
-		return ADD_EDGE;
-
-	} else if ( strcmp(commands, "remove edge") == 0) {
-
-		return REMOVE_EDGE;
+		fscanf(stdin, "%s", commands);
+		if (strcmp(commands, "edge") == 0) {
+			return REMOVE_EDGE;
+		}
 
 	} else if ( strcmp(commands, "check") == 0) {
 
@@ -182,14 +235,12 @@ int get_command_indexes(char *commands) {
 
 		return HELP;
 
-	} else if (strcmp(commands, "add") == 0) {
-
-		return ADD;
-	}
-	else if (strcmp(commands, "quit") == 0) {
+	} else if (strcmp(commands, "quit") == 0) {
 
 		return QUIT;
+
 	} else if (strcmp(commands, "display") == 0) {
+
 		return DISPLAY;
 	}
 
@@ -210,9 +261,9 @@ void display_help() {
 
 void free_names() {
 
-	/*int count;
+	int count;
 	for (count = 0; count < MAX_NUMBER_OF_VERTICES; count++) {
-		free(vertex_name[count]);
-	}*/
+		free(vertex_names[count]);
+	}
 
 }
