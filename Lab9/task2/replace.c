3e4a273410e707597345a4eafc2975139699b078
Unknown <darius.jones1018@gmail.com>
2018-03-31 10:30:19 -0700

    Added the fucntions for the linked list.

--- Lab9/task2/replace.c
+++ Lab9/task2/replace.c
@@ -10,66 +10,77 @@
 
 enum {HIT = 0, FAULT_SPACE, FAULT_NO_SPACE};
 
-typedef struct {
+typedef struct node NODE;
+struct node{
 	int data;
 	NODE *next;
 	NODE *prev;
-} NODE;
+};
+
+NODE *HEAD;
+
 
 int maximum_capacity;
 int current_capacity;
 int page_fault_count;
 
 
-void remove_last_node(/**/);
-void replace_head_node(/**/);
-void swap_node(/**/);
+void remove_tail_node(NODE *HEAD);
+void replace_head_node(int new_data);
+void swap_head_data(NODE *HEAD, NODE *to_swap);
+NODE* create_new_node(int new_data);
+void print_working_set();
 
 // int search_list(/**/); search list for
-// void create_list(/**/); maybe
-// void 
+
 
 int main(int argc, char *argv[]) {
 
 	return 0;
 }
 
+NODE* create_new_node(int new_data){
+	NODE *new_node = (NODE*)malloc(sizeof(NODE));
+	new_node->data = new_data;
+	new_node->prev = NULL;
+	new_node->next = NULL;
+	return new_node;
+}
 
+void replace_head_node(int new_data){
+	NODE *new_node = create_new_node(new_data);
 
-/*
-int search_list(???){
-
-	
-	int status;
-
-	while( current node != NULL){
-		
-
-		if (current.data ==  ){  found
-
-			status = HIT;
-			break;
-
-		}
-
-		traverse list
-
+	if(HEAD == NULL){
+		HEAD = new_node;
+		return;
 	}
 
-	if( found == fales &&  currnent_capacity == maximum_capacity){
-
-		status = FAULT_NO_SPACE;
-		page_fault_count++;
-
-	} else if(found == fales &&  currnent_capacity < maximum_capacity){
-
-		status = FAULT_SPACE;
-		page_fault_count++;
-	
-	}
-	
-	
-	return status;
+	HEAD->prev = new_node;
+	new_node->next = HEAD;
+	HEAD = new_node;
 
 }
-*/
+
+void remove_tail_node(NODE *HEAD){
+
+	while(HEAD != NULL){
+		HEAD = HEAD->next;
+	}
+	
+	if(HEAD->next == NULL){
+		HEAD = NULL;
+	}else{
+		HEAD->prev->next = NULL;
+	}
+
+}
+
+void swap_head_data(NODE *HEAD, NODE *to_swap){
+	int temp_data = HEAD->data;
+	HEAD->data = to_swap->data;
+	to_swap->data = temp_data;
+}
+
+void print_working_set(){
+
+}
