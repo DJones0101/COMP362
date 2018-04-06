57840e2ca40ff17e594cb36c9f84def8a57ceee5
Unknown <darius.jones1018@gmail.com>
2018-04-06 14:05:08 -0700

    added bitvector manipulation functions.

--- Project1/task1/filesystem.c
+++ Project1/task1/filesystem.c
@@ -4,6 +4,10 @@
 * 4/5/2018
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
 #define BLOCK_SIZE 256
 #define MAX_NAME_LENGTH 128
 #define DATA_SIZE 254
@@ -12,35 +16,31 @@
 typedef char data_t;
 typedef unsigned short index_t;
 
-typedef enum
-{
-   DIR,
-   FILE,
-   INDEX,
-   DATA
-} NODE_TYPE;
+typedef enum {DIR_ND, FILE_ND, INDEX_ND, DATA_ND} NODE_TYPE;
 
-typedef struct fs_node
-{
+typedef struct fd_node {
+
    char name[MAX_NAME_LENGTH];
-   time_t creation; 
-   time_t last_access 
-   time_t last_modification; 
-   mode_t access_rights; 
-   unsigned short owner_id; 
+   time_t creation;
+   time_t last_access;
+   time_t last_modification;
+   mode_t access_rights;
+   unsigned short owner_id;
    unsigned short size;
    index_t block_ref;
-} FS_NODE;
 
-typedef struct node
-{
+} FD_NODE;
+
+typedef struct node {
+
    NODE_TYPE type;
-   union
-   {
-      FS_NODE file_desc;
+
+   union {
+      FD_NODE file_desc;
       data_t data[DATA_SIZE];
       index_t index[INDEX_SIZE];
    } content;
+
 } NODE;
 
 // storage blocks
@@ -54,4 +54,64 @@ void file_create();
 void directory_create();
 void file_delete();
 void directory_delete();
+// bitvector operations
+void display_bitvector();
+int set_bit(char byte, int bit_position);
+bool test_bit(char byte, int bit_position);
+int clear_bit(char byte, int bit_position);
+void find_empty_bit();
+int get_bit(char byte, int bit_position);
 
+int main(int argc, char *argv[]) {
+   file_system_create();
+   display_bitvector();
+   return 0;
+}
+
+
+void file_system_create() {
+
+   bitvector = calloc(BLOCK_SIZE, sizeof(char));
+   memory = malloc( sizeof(NODE) * 65536);
+
+
+}
+
+
+void display_bitvector(){
+
+   int index, bit_position; 
+   int char_size = 8;
+
+   for(index = 0; index < BLOCK_SIZE; index++){
+
+      printf("%d : ", index);
+
+      for(bit_position = 0; bit_position < char_size; bit_position++){
+
+         printf("%d ",get_bit(bitvector[index],bit_position ));
+      }
+
+      printf("\n");
+   }
+}
+
+int get_bit(char byte, int bit_position){
+   int mask = 0x1;
+   return (byte & (mask << (bit_position - 1)));
+}
+
+int set_bit(char byte, int bit_position){
+   int mask = 0x1;
+   return (byte | (mask << bit_position));
+}
+
+int clear_bit(char byte, int bit_position){
+   int mask = ~(0x1 << bit_position);
+   return (byte & mask);
+}
+
+bool test_bit(char byte, int bit_position){
+   int mask = 0x1;
+   return ((byte & (mask << bit_position)) == 1) ? true : false;
+}
