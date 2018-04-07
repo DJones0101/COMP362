d466482216cced1a05fc5481ad43d39ab2e69fe3
Unknown <darius.jones1018@gmail.com>
2018-04-07 11:13:36 -0700

    Fixed bit manipulation bugs.

--- Project1/task1/filesystem.c
+++ Project1/task1/filesystem.c
@@ -11,6 +11,7 @@
 #include <string.h>
 
 #define BLOCK_SIZE 256
+#define TWOTOTHESIXTEENTH 65536
 #define MAX_NAME_LENGTH 128
 #define DATA_SIZE 254
 #define INDEX_SIZE 127
@@ -48,8 +49,7 @@ typedef struct node {
 
 // storage blocks
 
-NODE *memory; // allocate 2^16 blocks (in init)
-
+NODE *memory[TWOTOTHESIXTEENTH]; // allocate 2^16 blocks (in init)
 char *bitvector; // allocate space for managing 2^16 blocks (in init) for managing 2^16 blocks (in init)
 
 void file_system_create();
@@ -58,32 +58,42 @@ void directory_create();
 void file_delete();
 void directory_delete();
 void create_superblock();
-int find_memory_index();
-void mark_in_memory(NODE *node);
+int find_empty_memory_index();
+void place_in_memory(NODE *node);
+void allocate_blocks();
 // bitvector operations
 void display_bitvector();
 void mark_in_bitvector(char *bitvector);
-char set_bit(char byte, int bit_position);
-char clear_bit(char byte, int bit_position);
-bool get_bit(char byte, int bit_position);
-int find_empty_bit(char *bitvector);
+char set(char byte, int bit_position);
+char clear(char byte, int bit_position);
+bool is_bit_set(char byte, int bit_position);
+void mark_empty_bit(char *bitvector);
+
 
 int main(int argc, char *argv[]) {
    file_system_create();
-   display_bitvector();
+   //display_bitvector();
    return 0;
 }
 
 
 void file_system_create() {
 
-   bitvector = calloc(BLOCK_SIZE, sizeof(char));
-   memory = malloc( sizeof(NODE) * 65536);
-
+   allocate_blocks();
    create_superblock();
 }
 
-void create_superblock(){
+
+void allocate_blocks() {
+
+   bitvector = calloc(BLOCK_SIZE, sizeof(char));
+   int count;
+   for (count = 0; count < TWOTOTHESIXTEENTH; count++) {
+      memory[count] = malloc(sizeof(NODE));
+   }
+}
+
+void create_superblock() {
 
    time_t start = time(NULL);
 
@@ -96,42 +106,32 @@ void create_superblock(){
    SUPERBLOCK->content.file_desc.last_modification = start;
    SUPERBLOCK->content.file_desc.last_access = start;
    SUPERBLOCK->content.file_desc.access_rights = 0000;
-   SUPERBLOCK->content.file_desc.size = 0;
    SUPERBLOCK->content.file_desc.block_ref = 0;
-
+   SB_INDEX_ND->content.file_desc.block_ref = 1;
    SB_INDEX_ND->type =  INDEX_ND;
-   //SB_INDEX_ND->content.
+   place_in_memory(SUPERBLOCK);
+   place_in_memory(SB_INDEX_ND);
 
-   //put_in_memory(SUPERBLOCK);
-   //put_in_memory(SB_INDEX_ND);
 
-   set_bit(bitvector[0],1);
 
 }
 
-void mark_in_bitvector(char *bitvector){
+void place_in_memory(NODE *node) {
 
-   int bit_position = find_empty_bit(bitvector);
-}
-
-void mark_in_memory(NODE *node){
-
-   int mem_index = find_memory_index();
-  
-   if(mem_index != ERROR){
-      memory[mem_index] = *node;
-
+   int index = find_empty_memory_index();
+   if (index != ERROR) {
+      memory[index] = node;
+      mark_empty_bit(bitvector);
    }else{
-      printf("ERROR: could not place a node in memory\n");
+      printf("ERROR: in place_in_memory\n");
    }
 }
 
-
-int find_memory_index(){
+int find_empty_memory_index() {
    int index;
 
-   for(index = 0; index < 65536; index++){
-      if(memory[index] == NULL){
+   for (index = 0; index < 65536; index++) {
+      if (memory[index] != NULL) {
          return index;
       }
    }
@@ -139,7 +139,27 @@ int find_memory_index(){
    return ERROR;
 }
 
+void mark_empty_bit(char *bitvector) {
 
+   int bv_index;
+   int bit_position;
+   int char_size = 8;
+
+   for (bv_index = 0; bv_index < BLOCK_SIZE; bv_index++) {
+
+      for (bit_position = 0; bit_position < char_size; bit_position++) {
+
+         if (is_bit_set(bitvector[bv_index], bit_position) == false) {
+
+            bitvector[bv_index] = set(bitvector[bv_index], bit_position);
+            return;
+
+         }
+      }
+   }
+
+
+}
 
 void display_bitvector() {
 
@@ -152,13 +172,7 @@ void display_bitvector() {
 
       for (bit_position = 0; bit_position < char_size; bit_position++) {
 
-         if(get_bit(bitvector[index], bit_position ) == true){
-            printf("TRUE\n");
-         }else if(get_bit(bitvector[index], bit_position ) == false){
-            printf("FALSE\n");
-         }
-
-         printf("%u ", get_bit(bitvector[index], bit_position ));
+         printf("%u ", is_bit_set(bitvector[index], bit_position ));
       }
 
       printf("\n");
@@ -167,38 +181,17 @@ void display_bitvector() {
 
 
 
-bool get_bit(char byte, int bit_position) {
-   return ((byte & ( 1 << bit_position)) != 0);
+bool is_bit_set(char byte, int bit_position) {
+   return ((byte & (1 << bit_position)) != 0);
 }
 
-char set_bit(char byte, int bit_position) {
-    return (byte | (1 << bit_position));
+char set(char byte, int bit_position) {
+   return (byte | (1 << bit_position));
 }
 
-char clear_bit(char byte, int bit_position) {
-   int mask = ~(1 << bit_position);
-   return (byte & mask);
+char clear(char byte, int bit_position) {
+   return (byte & (~(1 << bit_position)));
 }
-
-void mark_empty_bit(char *bitvector){
-   int bv_index, bit_position;
-   int char_size = 8;
-
-   for(bv_index = 0; bv_index < BLOCK_SIZE; bv_index++){
-      
-      for(bit_position = 0; bit_position < char_size; bit_position++){
-         
-         if(get_bit(bitvector[bv_index], bit_position) == true){
-            set_bit(bitvector[bv_index], bit_position);
-            return;
-
-         }
-      }
-   }
-
-
-}
-
 
 
 
