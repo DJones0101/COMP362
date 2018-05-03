99ba8a7123170f0b1f8ae7ac39a4d4526d6e68be
Unknown <darius.jones1018@gmail.com>
2018-05-03 11:10:53 -0700

    added info on how to install linux headers. For kernel 14.14  commented out #include <asm/uaccess.h>

---
+++ Project2/task3/disk.c
@@ -0,0 +1,104 @@
+/*
+* Darius Jones
+* Project 2 task 1
+* 4/26/2018
+*/
+
+#include "disk.h"
+
+
+int log_to_phys(int logical_address, physical_address *phys_address) {
+
+
+	/*C = LBA ÷ (HPC × SPT)
+	  H = (LBA ÷ SPT) mod HPC
+	  S = (LBA mod SPT) + 1*/
+
+	phys_address->cyl = logical_address / (NUM_OF_HEADS * NUM_OF_SECTS);
+	phys_address->head = (logical_address / NUM_OF_SECTS) % NUM_OF_HEADS ;
+	phys_address->sect = (logical_address % NUM_OF_SECTS) + 1;
+
+	if ((phys_address->cyl <= NUM_OF_CYLS) && (phys_address->head <= NUM_OF_HEADS) && (phys_address->sect <= NUM_OF_SECTS)) {
+		return VALID;
+	}
+
+	return INVALID;
+
+}
+
+int phys_to_log(physical_address *phys_address) {
+
+	/*LBA = (C × HPC + H) × SPT + (S - 1)*/
+	return (phys_address->cyl * NUM_OF_HEADS + phys_address->head) *  NUM_OF_SECTS + (phys_address->sect - 1);
+
+}
+
+int read(int logical_block_num, int num_of_blocks, void **buffer) {
+
+	if (is_valid(logical_block_num, num_of_blocks) == true) {
+		memset((char*)buffer,'\0', strlen((char*)buffer));
+		physical_address phaddr;
+		log_to_phys(logical_block_num, &phaddr);
+		int count;
+
+		for (count = 0; count < num_of_blocks; count++) {
+			strcpy(((char *)buffer), &(disk[phaddr.cyl][phaddr.head][phaddr.sect][count]));
+		}
+		 
+		return 0;
+
+
+	}
+
+	return -1;
+}
+
+int write(int logical_block_num, int num_of_sectors, void *buffer) {
+
+	if (is_valid(logical_block_num, num_of_sectors) == true) {
+
+		physical_address phaddr;
+
+		log_to_phys(logical_block_num, &phaddr);
+		char *value = malloc(sizeof((char*)buffer));
+		strcpy(value, buffer);
+		int count;
+		for (count = 0; count < num_of_sectors; count++) {
+			strcpy(&(disk[phaddr.cyl][phaddr.head][phaddr.sect][count]), value);
+
+		}
+		memset(buffer,'\0', strlen(buffer)); 
+		free(value);
+		return 0;
+	}
+	return -1;
+}
+
+
+bool is_valid(int logical_block_num, int num_of_sects) {
+
+	if ((logical_block_num < 0 || logical_block_num >= MAX_LOGICAL_SECTOR)
+	        || (num_of_sects < 0 || num_of_sects > NUM_OF_SECTS)) {
+
+		return false;
+	}
+	return true;
+}
+
+void random_string(char *buffer, int length) {
+
+
+	char alpha[50] = "abcdefghijklmnopqrstuvwxyz";
+
+	int count;
+
+
+	for (count = 0; count < (length - 1); ++count) {
+
+		buffer[count] = alpha[rand() % 26];
+	}
+	buffer[length + 1] = '\0';
+
+}
+
+
---
+++ Project2/task3/sim_dev.h
@@ -0,0 +1,10 @@
+#ifndef __SIM_DEV_H_
+#define __SIM_DEV_H_
+
+#define IOCTL_SIM_DEV_WRITE _IOR(0, 1, int)
+#define IOCTL_SIM_DEV_READ _IOR(0, 2, int)
+copy_to_user(buf, storage, count);
+copy_from_user(storage, buf, count);
+
+#endif
+
